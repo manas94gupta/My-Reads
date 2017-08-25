ad0018bed7b5be5eea1a36eed90d5bc5fc4cd8b1
Manas <manas94gupta@gmail.com>
2017-08-26 00:35:36 +0530

    refactor: refactor bookslist component to be a stateless functional component

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BookView from './BookView.js'
 
-class BooksList extends Component {
-    render() {
-        return (
-            <div className="list-books-content">
-                <div>
-                    <div className="bookshelf">
-                        <h2 className="bookshelf-title">Currently Reading</h2>
-                        <div className="bookshelf-books">
-                            <ol className="books-grid">
-                                {this.props.books.filter((book) => book.shelf === "currentlyReading").map((book) => <BookView key={book.id} book={book} onChangeShelf={this.props.onChangeShelf} />)}
-                            </ol>
-                        </div>
+function BooksList (props) {
+    return (
+        <div className="list-books-content">
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {props.books.filter((book) => book.shelf === "currentlyReading").map((book) => <BookView key={book.id} book={book} onChangeShelf={props.onChangeShelf} />)}
+                        </ol>
                     </div>
-                    <div className="bookshelf">
-                        <h2 className="bookshelf-title">Want to Read</h2>
-                        <div className="bookshelf-books">
-                            <ol className="books-grid">
-                                {this.props.books.filter((book) => book.shelf === "wantToRead").map((book) => <BookView key={book.id} book={book} onChangeShelf={this.props.onChangeShelf} />)}
-                            </ol>
-                        </div>
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {props.books.filter((book) => book.shelf === "wantToRead").map((book) => <BookView key={book.id} book={book} onChangeShelf={props.onChangeShelf} />)}
+                        </ol>
                     </div>
-                    <div className="bookshelf">
-                        <h2 className="bookshelf-title">Read</h2>
-                        <div className="bookshelf-books">
-                            <ol className="books-grid">
-                                {this.props.books.filter((book) => book.shelf === "read").map((book) => <BookView key={book.id} book={book} onChangeShelf={this.props.onChangeShelf} />)}
-                            </ol>
-                        </div>
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {props.books.filter((book) => book.shelf === "read").map((book) => <BookView key={book.id} book={book} onChangeShelf={props.onChangeShelf} />)}
+                        </ol>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BooksList
